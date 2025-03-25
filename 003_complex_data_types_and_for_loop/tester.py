9706556f4fe4d616c50c44338fd88e5242cfe21a
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-03-25 12:19:23 +0200

    List tuple set

---
+++ 003_complex_data_types_and_for_loop/tester.py
@@ -0,0 +1,102 @@
+# # empty = []  # List (list())
+# # empty = list()
+# # print(type(empty))
+# #
+# # random_list = [123, 12.32, 'Hello world', True, None, [1, 2, [3, 4, 5], 6], True, True, True]
+# # print(len(random_list))
+# # print(random_list[5][2][2] ** 2)
+# # print(random_list[2].upper()[0:5].lower())
+# #
+# # a = 10
+# # b = 'Hello'
+# # c = [a, b]
+# # print(c)
+#
+# # courses = ['English', 'Math', 'Physics', 'Programming']
+# # print(courses[1])
+# # courses[1] = 'Art'
+# # print(courses)
+# # courses[1:3] = ['History']
+# # print(courses)
+# #
+# # print(list('Hello world!'))
+#
+# cities = ['Tallinn', 'London', 'Madrid', 'Oslo', 'tallinn', '123', 'Oslo', 'london']
+# nums = [1, 4, 6, 3, 8, 3, 4, 2, 11, 111]
+#
+# # cities.append('Washington')
+# # print(cities)
+# # cities.insert(0, 'New-York')
+# # print(cities)
+# # cities.extend(['Rio', 'Beijing'])
+# # print(cities)
+# #
+# # cities.remove('Madrid')
+# # print(cities)
+# # deleted = cities.pop(0)
+# # print(cities)
+# # print(deleted)
+#
+# # cities.reverse()
+# # print(cities)
+# # print(cities[::-1])
+# # cities.sort(reverse=True)
+# # print(cities)
+# # nums.sort()
+# # print(nums)
+#
+# # print([1, 2, 3] + [4, 5, 6])
+#
+# # print(cities.index('Oslo'))
+# # print(cities.count('Oslo'))
+# #
+# # sample = 'Hello people of planet Earth, good bye!'
+# # print(sample.split(', '))
+# # print('2000-12-02'.split('-'))
+# # print('\n'.join(cities))
+#
+# # a = 5
+# # b = a
+# # a += 10
+# # print(a, b)
+#
+# # a = 'Hello'
+# # b = a
+# # a += 'World'
+# # print(a, b)
+#
+# a = [1, 2, 3]
+# b = a.copy()
+# a.append(4)
+# b.append(5)
+# print(a, b)
+# print(id(a))
+# print(id(b))
+#
+# print(sum(nums))
+# print(min(nums))
+# print(max(nums))
+# print(min(cities))
+#
+
+# empty = (1,)
+# print(type(empty))
+
+# tup1 = (1, 2, 3, 4, 5, 6)
+# print(tup1[2:5] + (1, 23, 43))
+# tup1 = list(tup1)
+# print(tuple(tup1))
+# print(sum(tup1, 1000))
+
+courses = {'Math', 'Art', 'Physics', 'Programming', 'Art', 'Art'}
+print(courses)
+
+set1 = {'Math', 'Art', 'Physics'}
+set2 = {'Math', 'Physics', 'History'}
+
+print(set1.difference(set2))
+print(set2.difference(set1))
+
+print(set1.intersection(set2))
+
+print(set1.symmetric_difference(set2))
